eecd4d29c907a0f051ec6645b0b04c63a2c82abe
Mikel <mikelk96@gmail.com>
2025-03-24 14:14:02 +0200

    shimmy

--- VisualizerRoutine.py
+++ VisualizerRoutine.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 from Dataset import Dataset
 
 DATA_DIR = "./cifar-10-batches-py"
@@ -12,21 +11,19 @@ class VisualizerRoutine:
         self.batch_size = batch_size
 
     def Run(self):
-        if self.dataset.__len__() == 0:
+        if len(self.dataset) == 0:
             print("Dataset not loaded. Call Load() first.")
             return
 
-        for i in range(0, len(self.dataset), self.batch_size):
-            images = self.dataset.data[i:i+self.batch_size].numpy().transpose(0, 2, 3, 1)  # Convert to (batch, H, W, C)
-            images = (images * 255).astype(np.uint8)  # Convert to uint8 for OpenCV
-            
-            fig, axes = plt.subplots(1, len(images), figsize=(10, 5))
-            for img, ax in zip(images, axes):
-                ax.imshow(cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
-                ax.axis("off")
-            
-            plt.show()
-            input("Press Enter to continue...")
+        for iterImage, iterImageIndex in self.dataset:
+
+            imageNumpy = iterImage.numpy().transpose(1, 2, 0)  # bgr to rgb
+            imageNumpy = (imageNumpy * 255).astype(np.uint8)
+
+            cv2.imshow("CIFAR-10 Image", cv2.cvtColor(imageNumpy, cv2.COLOR_RGB2BGR))
+            cv2.waitKey(0)
+
+            cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
