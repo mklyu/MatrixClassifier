62a29f01efac0cd4a33f8d2dc4e237e388f4321a
Mikel <mikelk96@gmail.com>
2025-03-24 16:09:53 +0200

    resolve opecv and pyqt5 problem

---
+++ Util/OpencvPyqt5Fix.py
@@ -0,0 +1,13 @@
+
+def FUNC_FIX_OPENCV_PYQT5_XCB():
+    import os, sys
+    ci_build_and_not_headless = False
+    try:
+        from cv2.version import ci_build, headless
+        ci_and_not_headless = ci_build and not headless
+    except:
+        pass
+    if sys.platform.startswith("linux") and ci_and_not_headless:
+        os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH")
+    if sys.platform.startswith("linux") and ci_and_not_headless:
+        os.environ.pop("QT_QPA_FONTDIR")
